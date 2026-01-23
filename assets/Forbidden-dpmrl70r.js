01114df3a8d191a62de6fcce7599e376393253f4
Santiago <santi22gilli@gmail.com>
2026-01-23 14:26:08 -0300

    Updates

--- assets/Forbidden-dpmrl70r.js
+++ assets/Forbidden-dpmrl70r.js
@@ -1,4 +1,4 @@
-import{c as t,j as e,L as a}from"./index-DLNJyO3D.js";/**
+import{c as t,j as e,L as a}from"./index-Bc1ZYu_m.js";/**
  * @license lucide-react v0.309.0 - ISC
  *
  * This source code is licensed under the ISC license.
