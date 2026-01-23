a48c561a49112a05b709e6bfccd18e1f3d2ba593
Santiago <santi22gilli@gmail.com>
2026-01-22 23:33:22 -0300

    Updates

--- assets/Forbidden-DffmgkYi.js
+++ assets/Forbidden-DffmgkYi.js
@@ -1,4 +1,4 @@
-import{d as t,j as e,L as a}from"./index-DG1EOWbF.js";/**
+import{d as t,j as e,L as a}from"./index-86quVG3u.js";/**
  * @license lucide-react v0.309.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/Maintenance-CLmZXVAO.js
+++ assets/Maintenance-CLmZXVAO.js
@@ -1,4 +1,4 @@
-import{d as a,j as e}from"./index-DG1EOWbF.js";/**
+import{d as a,j as e}from"./index-86quVG3u.js";/**
  * @license lucide-react v0.309.0 - ISC
  *
  * This source code is licensed under the ISC license.
