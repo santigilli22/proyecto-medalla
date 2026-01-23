189595de8bf20adc1b8b22010802ecd3b044b96d
Santiago <santi22gilli@gmail.com>
2026-01-22 23:39:38 -0300

    Updates

--- assets/Maintenance-BCnQJMGf.js
+++ assets/Maintenance-BCnQJMGf.js
@@ -1,4 +1,4 @@
-import{d as a,j as e}from"./index-86quVG3u.js";/**
+import{d as a,j as e}from"./index-D4xY1QEy.js";/**
  * @license lucide-react v0.309.0 - ISC
  *
  * This source code is licensed under the ISC license.
