5aef74ae6dde12c6bae0109169cd94541ebe28d1
Santiago <santi22gilli@gmail.com>
2026-01-23 21:14:15 -0300

    Updates

--- assets/Forbidden-DAclh-Ea.js
+++ assets/Forbidden-DAclh-Ea.js
@@ -1,4 +1,4 @@
-import{c as t,j as e,L as a}from"./index-CW-kpD5t.js";/**
+import{c as t,j as e,L as a}from"./index-ZRIQJrQg.js";/**
  * @license lucide-react v0.309.0 - ISC
  *
  * This source code is licensed under the ISC license.
