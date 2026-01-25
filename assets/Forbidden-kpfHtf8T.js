d93652370a588a03ee6f8d211b1183a256aa568c
Santiago <santi22gilli@gmail.com>
2026-01-24 21:50:56 -0300

    Updates

--- assets/Forbidden-kpfHtf8T.js
+++ assets/Forbidden-kpfHtf8T.js
@@ -1,4 +1,4 @@
-import{c as t,j as e,L as a}from"./index-DVhY6XOb.js";/**
+import{c as t,j as e,L as a}from"./index-oQX6RfSt.js";/**
  * @license lucide-react v0.309.0 - ISC
  *
  * This source code is licensed under the ISC license.
