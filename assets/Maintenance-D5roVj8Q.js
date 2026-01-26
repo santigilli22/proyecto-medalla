db9de3ef963ab1e1884b779d6eecdc98dca93e69
Santiago <santi22gilli@gmail.com>
2026-01-26 09:51:01 -0300

    Updates

--- assets/Maintenance-D5roVj8Q.js
+++ assets/Maintenance-D5roVj8Q.js
@@ -1,4 +1,4 @@
-import{c as a,j as e}from"./index-oQX6RfSt.js";/**
+import{c as a,j as e}from"./index-DOcSsHTJ.js";/**
  * @license lucide-react v0.309.0 - ISC
  *
  * This source code is licensed under the ISC license.
